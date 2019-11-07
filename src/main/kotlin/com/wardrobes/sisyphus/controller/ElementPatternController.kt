2c1ce3a97d52614f5ec1b8f580cd695a7bd96f44
Rafał <rafalhaglauer@gmail.com>
2019-11-07 19:38:59 +0100

    added element pattern controller

---
+++ src/main/kotlin/com/wardrobes/sisyphus/controller/ElementPatternController.kt
@@ -0,0 +1,43 @@
+package com.wardrobes.sisyphus.controller
+
+import com.wardrobes.sisyphus.domain.pattern.ElementPattern
+import com.wardrobes.sisyphus.domain.pattern.WardrobePattern
+import com.wardrobes.sisyphus.model.ElementPatternRepository
+import com.wardrobes.sisyphus.model.WardrobePatternRepository
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/pattern")
+class ElementPatternController(
+    private val elementPatternRepository: ElementPatternRepository,
+    private val wardrobePatternRepository: WardrobePatternRepository
+) {
+
+    @GetMapping("/wardrobe/{wardrobeId}/elements")
+    fun getAll(@PathVariable wardrobeId: Long): List<ElementPattern> = elementPatternRepository.findAll().toList().filter {
+        it.wardrobe.id == wardrobeId
+    }
+
+    @GetMapping("/element/{id}")
+    fun get(@PathVariable id: Long) = elementPatternRepository.findById(id).get()
+
+    @PostMapping("/wardrobe/{wardrobeId}/elements")
+    fun create(@PathVariable wardrobeId: Long, @RequestBody element: ElementPattern) {
+        val wardrobePattern = wardrobePatternRepository.findById(wardrobeId).get()
+        elementPatternRepository.save(element.apply { wardrobe = wardrobePattern })
+    }
+
+    @PutMapping("/element/{id}")
+    fun update(@PathVariable id: Long, @RequestBody element: ElementPattern) {
+        elementPatternRepository.save(get(id).apply {
+            name = element.name
+            length = element.length
+            width = element.width
+            height = element.height
+        })
+    }
+
+    @DeleteMapping("/element/{id}")
+    fun delete(@PathVariable id: Long) = elementPatternRepository.deleteById(id)
+
+}
